2e8bc47218b4bfa7fff565080139e7d02b9a1da5
Ayushi K <kandpalharayu@gmail.com>
2023-06-29 08:45:57 +0530

    Add files via upload
    Solution to a C program problem

---
+++ 5_students_array.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+	printf("Hello Harshit");
+	printf("%d",45);
+}
+
+// int main(){
+	
+// 	int s1[4],s2[4],s3[4],s4[4],s5[4];
+// 	int av1=0,av2=0,av3=0,av4=0,av5=0;
+// 	float tot[4];
+	
+// 	for(int i =0;i<=3;i++){
+		
+// 		printf("\nEnter subject %d marks for all 5 students : ",i+1);
+// 		scanf("%d %d %d %d %d",&s1[i],&s2[i],&s3[i],&s4[i],&s5[i]);
+// 		av1+=s1[i];
+// 		av2+=s2[i];
+// 		av3+=s3[i];
+// 		av4+=s4[i];
+// 		av5+=s5[i];
+		
+// 		tot[i]= s1[i]+s2[i]+s3[i]+s4[i]+s5[i];
+// 		printf("\nThe average of subject %d is %f:",i+1,tot[i]/5);
+// 	}
+	
+// 	printf("\nAvg of stud 1%f",av1/4);
+// 	printf("\nAvg of stud 2%f",av2/4);
+// 	printf("\nAvg of stud 3%f",av3/4);
+// 	printf("\nAvg of stud 4%f",av4/4);
+// 	printf("\nAvg of stud 5%f",av5/4);
+	
+// }
